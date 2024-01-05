563518e7661da3ef3858f428f4dc154225449d6c
unknown <jasonbailleul59@laposte.net>
2024-01-05 10:59:01 +0100

    connexion et redirection2

--- src/main/java/dao/ClientDao.java
+++ src/main/java/dao/ClientDao.java
@@ -1,14 +1,14 @@
 package dao;
 
 import model.Client;
+import model.Pays;
+import model.Role;
+import model.Ville;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
 
-public class ClientDao extends DAO<Client, Integer>{
+public class ClientDao extends DAO<Client, Integer> {
     @Override
     public Client getByID(Integer integer) {
         return null;
@@ -47,20 +47,20 @@ public class ClientDao extends DAO<Client, Integer>{
     public boolean update(Client client) {
         String sqlRequest = "update Client set denominationSociale = ?, mailClient = ?," +
                 "numeroRueClient = ?, libelleRueClient = ?, completmentAdresseClient = ?" +
-                "codePostalClient = ?, numeroTva = ?, numTelClient = ?"+
+                "codePostalClient = ?, numeroTva = ?, numTelClient = ?" +
                 "where id_client = ?";
-        try(PreparedStatement preparedStatement = connexion.prepareStatement(sqlRequest, Statement.RETURN_GENERATED_KEYS)){
-            preparedStatement.setString(1,client.getDenominationSociale());
-            preparedStatement.setString(2,client.getMailClient());
-            preparedStatement.setString(3,client.getNumeroRueCLient());
+        try (PreparedStatement preparedStatement = connexion.prepareStatement(sqlRequest, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, client.getDenominationSociale());
+            preparedStatement.setString(2, client.getMailClient());
+            preparedStatement.setString(3, client.getNumeroRueCLient());
             preparedStatement.setString(4, client.getLibelleRueClient());
             preparedStatement.setString(5, client.getComplementAdresseClient());
             preparedStatement.setString(6, client.getCodePostalClient());
             preparedStatement.setString(7, client.getNumeroTva());
-            preparedStatement.setString(8,client.getNumTelClient());
+            preparedStatement.setString(8, client.getNumTelClient());
             preparedStatement.executeUpdate();
             return true;
-        }catch (SQLException e){
+        } catch (SQLException e) {
             return false;
         }
     }
@@ -68,5 +68,29 @@ public class ClientDao extends DAO<Client, Integer>{
     @Override
     public boolean delete(Client objet) {
         return false;
+    }
+
+    public Client searchConnexion(String email, String password) {
+        Client client = null;
+
+        String strCmd = "SELECT c.denominationSociale,c.mailClient,c.numeroRueClient,c.libelleRueClient,c.complementAdresseClient,c.codePostalClient,c.numeroTva,c.numTelClient,c.passwordClient,r.id_role, r.libelleRole, v.id_ville, v.libelleVille FROM Client as c JOIN Role as r ON c.id_role = r.id_role JOIN Ville as v ON c.id_ville = v.id_ville WHERE mailClient = ? AND passwordClient = ? ";
+        PreparedStatement stmt;
+        try  {
+            stmt = connexion.prepareStatement(strCmd);
+            stmt.setString(1,email);
+            stmt.setString(2,password);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next())
+                client = new Client(rs.getString(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5), rs.getString(6),
+                        rs.getString(7), rs.getString(8), rs.getString(9),
+                        new Role(rs.getInt(10), rs.getString(11)), new Ville(rs.getInt(12), rs.getString(13)));
+
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return client;
     }
 }
--- src/main/java/fr/rapplication/rapp/HelloApplication.java
+++ src/main/java/fr/rapplication/rapp/HelloApplication.java
@@ -1,24 +1,66 @@
 package fr.rapplication.rapp;
 
-import bean.PaysBean;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    @FXML
+    private BorderPane rootLayout;
+    private Stage primaryStage;
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Reserve App");
+        this.primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/zen.png")));
+        this.primaryStage.setResizable(false);
+        initRootLayout();
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 780, 460);
+            LoginController loginController = fxmlLoader.getController();
+            loginController.setHelloApplication(this);
+            scene.getStylesheets().add(getClass().getResource("login.css").toExternalForm());
+            this.primaryStage.setScene(scene);
+            this.primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void switchClientView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("info-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 780, 460);
+        //scene.getStylesheets().add(getClass().getResource("login.css").toExternalForm());
+        Stage stage2 = new Stage();
+        stage2.setTitle("Reserve App - Mes infos");
+        stage2.setResizable(false);
+        stage2.setScene(scene);
+        stage2.show();
+
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
     }
 }
